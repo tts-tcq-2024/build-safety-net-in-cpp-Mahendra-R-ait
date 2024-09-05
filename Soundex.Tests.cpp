fe08c2a376178af59fe66fd5f1aabe86aefaba68
Mahendra R <83295768+Mahendra-R-ait@users.noreply.github.com>
2024-09-05 10:44:31 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,89 +1,77 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
- 
+
 // Test cases for the empty string check
 TEST(SoundexTest, HandlesEmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
- 
+
 // Test cases for the single character check
 TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("B"), "B000");
-    EXPECT_EQ(generateSoundex("C"), "C000");
-    EXPECT_EQ(generateSoundex("D"), "D000");
-    EXPECT_EQ(generateSoundex("E"), "E000");
-    EXPECT_EQ(generateSoundex("F"), "F000");
-    EXPECT_EQ(generateSoundex("G"), "G000");
-    EXPECT_EQ(generateSoundex("H"), "H000");
-    EXPECT_EQ(generateSoundex("I"), "I000");
-    EXPECT_EQ(generateSoundex("J"), "J000");
-    EXPECT_EQ(generateSoundex("K"), "K000");
-    EXPECT_EQ(generateSoundex("L"), "L000");
-    EXPECT_EQ(generateSoundex("M"), "M000");
-    EXPECT_EQ(generateSoundex("N"), "N000");
-    EXPECT_EQ(generateSoundex("O"), "O000");
-    EXPECT_EQ(generateSoundex("P"), "P000");
-    EXPECT_EQ(generateSoundex("Q"), "Q000");
-    EXPECT_EQ(generateSoundex("R"), "R000");
-    EXPECT_EQ(generateSoundex("S"), "S000");
-    EXPECT_EQ(generateSoundex("T"), "T000");
-    EXPECT_EQ(generateSoundex("U"), "U000");
-    EXPECT_EQ(generateSoundex("V"), "V000");
-    EXPECT_EQ(generateSoundex("W"), "W000");
-    EXPECT_EQ(generateSoundex("X"), "X000");
-    EXPECT_EQ(generateSoundex("Y"), "Y000");
-    EXPECT_EQ(generateSoundex("Z"), "Z000");
+    for (char c = 'A'; c <= 'Z'; ++c) {
+        std::string expected = std::string(1, c) + "000";
+        EXPECT_EQ(generateSoundex(std::string(1, c)), expected);
+    }
 }
- 
+
+
 // Test cases for the names with vowels and certain consonants that should be ignored
 TEST(SoundexTest, HandlesIgnoredCharacters) {
-    EXPECT_EQ(generateSoundex("AEIOU"), "A000");
-    EXPECT_EQ(generateSoundex("HW"), "H000");
+    std::vector<std::pair<std::string, std::string>> testCases = {
+        {"AEIOU", "A000"}, {"EAIOU", "E000"}, {"IAEOU", "I000"}, 
+        {"OAEIU", "O000"}, {"UAEIO", "U000"}, {"HW", "H000"}, 
+        {"WH", "W000"}
+    };
+    for (const auto& testCase : testCases) {
+        EXPECT_EQ(generateSoundex(testCase.first), testCase.second);
+    }
 }
- 
+
+
 // Test cases for the names with adjacent consonants that should be ignored
 TEST(SoundexTest, HandlesAdjacentConsonants) {
-    EXPECT_EQ(generateSoundex("BB"), "B100");
-    EXPECT_EQ(generateSoundex("CC"), "C200");
-    EXPECT_EQ(generateSoundex("DD"), "D300");
-    EXPECT_EQ(generateSoundex("FF"), "F100");
-    EXPECT_EQ(generateSoundex("GG"), "G200");
-    EXPECT_EQ(generateSoundex("JJ"), "J200");
-    EXPECT_EQ(generateSoundex("KK"), "K200");
-    EXPECT_EQ(generateSoundex("LL"), "L400");
-    EXPECT_EQ(generateSoundex("MM"), "M500");
-    EXPECT_EQ(generateSoundex("NN"), "N500");
-    EXPECT_EQ(generateSoundex("PP"), "P100");
-    EXPECT_EQ(generateSoundex("QQ"), "Q200");
-    EXPECT_EQ(generateSoundex("RR"), "R600");
-    EXPECT_EQ(generateSoundex("SS"), "S200");
-    EXPECT_EQ(generateSoundex("TT"), "T300");
-    EXPECT_EQ(generateSoundex("VV"), "V100");
-    EXPECT_EQ(generateSoundex("XX"), "X200");
-    EXPECT_EQ(generateSoundex("YY"), "Y000");
-    EXPECT_EQ(generateSoundex("ZZ"), "Z200");
+    std::map<char, std::string> soundexMapping = {
+        {'B', "B100"}, {'C', "C200"}, {'D', "D300"}, {'F', "F100"}, 
+        {'G', "G200"}, {'J', "J200"}, {'K', "K200"}, {'L', "L400"}, 
+        {'M', "M500"}, {'N', "N500"}, {'P', "P100"}, {'Q', "Q200"}, 
+        {'R', "R600"}, {'S', "S200"}, {'T', "T300"}, {'V', "V100"}, 
+        {'X', "X200"}, {'Y', "Y000"}, {'Z', "Z200"}
+    };
+
+    for (const auto& entry : soundexMapping) {
+        char consonant = entry.first;
+        std::string expected = entry.second;
+        EXPECT_EQ(generateSoundex(std::string(2, consonant)), expected);
+    }
 }
- 
+
+
 // Test cases for the longer names that require truncation
 TEST(SoundexTest, HandlesLongNames) {
-    EXPECT_EQ(generateSoundex("Devaraj"), "D123");
-    EXPECT_EQ(generateSoundex("Mahendra"), "M230");
-    EXPECT_EQ(generateSoundex("Balaji"), "B420");
-    EXPECT_EQ(generateSoundex("Singh"), "S520");
-    EXPECT_EQ(generateSoundex("Rajamouli"), "R254");
-     EXPECT_EQ(generateSoundex("Raina"), "R500");
+    EXPECT_EQ(generateSoundex("Aeiouhw"), "A000");
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T520");
+    EXPECT_EQ(generateSoundex("Pfister"), "P123");
+    EXPECT_EQ(generateSoundex("Ashworth"), "A263");
+    EXPECT_EQ(generateSoundex("Bobby"), "B100");
+    EXPECT_EQ(generateSoundex("Hildebrand"), "H431");
+    EXPECT_EQ(generateSoundex("Barack"), "B620");
+    EXPECT_EQ(generateSoundex("abcdefghijklmnopqrstuvwxyz"), "A123");
+    EXPECT_EQ(generateSoundex("RoBErt"), "R163");
+    EXPECT_EQ(generateSoundex("0"), "0000");
+    EXPECT_EQ(generateSoundex("1234"), "1000");
 }
- 
+
 // Test cases for the names with hyphens or spaces
 TEST(SoundexTest, HandlesHyphensAndSpaces) {
-    EXPECT_EQ(generateSoundex("Rama-Rao"), "R362");
-    EXPECT_EQ(generateSoundex("Dhinakaran-Koilpitchai"), "D526");
-    EXPECT_EQ(generateSoundex("Amithab Bachhan"), "A531");
+    EXPECT_EQ(generateSoundex("Raj-Kumar"), "R362");
+    EXPECT_EQ(generateSoundex("Dinesh-Karthik"), "D526");
+    EXPECT_EQ(generateSoundex("Vimak Kirshna"), "V632");
 }
- 
+
 // Test cases for the names with non-alphabetic characters
 TEST(SoundexTest, HandlesNonAlphabeticCharacters) {
-    EXPECT_EQ(generateSoundex("Jai'Balaya"), "J156");
-    EXPECT_EQ(generateSoundex("Mahendra@15"), "M620");
+    EXPECT_EQ(generateSoundex("Mahendra@"), "H500");
 }
